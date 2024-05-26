e80006fd86c7331c240d3c1c16850e5e5b713d84
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2024-05-26 13:21:08 -0500

    Initial commit

---
+++ src/test/java/com/globant/juandiegoespinosa/java_career_path_challenge/JavaCareerPathChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.juandiegoespinosa.java_career_path_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCareerPathChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
